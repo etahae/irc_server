dfcf3c97d4e39f5031ee950a8ceab741c49dcb4c
Taha Namir <tnamir@e2r2p10.1337.ma>
2022-12-29 03:54:09 +0100

    update: replies and errors codes

--- server.hpp
+++ server.hpp
@@ -5,6 +5,24 @@
 
 #include"client.hpp"
 
+// REPLIES
+#define RPL_LIST(channel, usersCount)			("322 * #" + channel + " " + usersCount)
+#define RPL_LISTEND								"323 * :End of LIST"
+
+
+
+// ERRORS
+# define ERR_NEEDMOREPARAMS(command)			("461 * " + command + ": Not enough parameters")
+# define ERR_ALREADYREGISTRED					"462 * :Unauthorized command (already registered)"
+# define ERR_PASSWDMISMATCH						"464 * :Password incorrect"
+# define ERR_NICKNAMEINUSE(nick)				("433 * " + nick + ":Nickname is already in use")
+# define ERR_RESTRICTED							"484 * :Your connection is restricted!"
+# define ERR_NOTONCHANNEL(channel)				("442 * " + channel + ":You're not on that channel")
+# define ERR_NOSUCHCHANNEL(channel)				("403 * #" + channel + ":No such channel")
+# define ERR_CHANOPRIVSNEEDED(channel)			("482 * #" + channel + ":You're not channel operator")
+# define ERR_USERNOTINCHANNEL(nick, channel)	("441 * " + nick + " #" + channel + ":They aren't on that channel")
+# define ERR_BADCHANMASK(channel)				("476 * #" + channel + ":Bad Channel Mask")
+
 class Server{
 	public :
 
