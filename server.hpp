98b8f7b580560feda074d3382b2eb7073092f7e7
Taha Namir <tnamir@e2r2p8.1337.ma>
2022-12-30 19:21:22 +0100

    add : commands.cpp | update : moved commands functions to commands.cpp files

--- server.hpp
+++ server.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #define VERIFIED "\033[0;32m • \033[0m"
+#define CONNECTED "\033[0;32m • \033[0m"
 #define DISCONNECTED "\033[0;31m • \033[0m"
 
 #include"client.hpp"
@@ -24,95 +25,78 @@
 // # define ERR_USERNOTINCHANNEL(nick, channel)	("441 * " + nick + " #" + channel + ":They aren't on that channel")
 // # define ERR_BADCHANMASK(channel)				("476 * #" + channel + ":Bad Channel Mask")
 
-class Server{
-	public :
+namespace irc
+{
+	class Server{
+		public :
 
-		string				password;
-		int					port_number;
-		int					_socket;
-		std::vector<Client>	clients;
-		struct sockaddr_in	server_addr;
+			string				password;
+			int					port_number;
+			int					_socket;
+			fd_set				r_socket, w_socket;
+			std::vector<Client>	clients;
+			struct sockaddr_in	server_addr;
 
-		~Server()
-		{
-			clients.clear();
-		}
+			~Server()
+			{
+				// clients.clear();
+			}
 
-		int fatal_error(std::string msg)
-		{
-			std::cerr << "error: " << msg << std::endl;
-			return EXIT_FAILURE;
-		}
+			int fatal_error(std::string msg)
+			{
+				std::cerr << "error: " << msg << std::endl;
+				return EXIT_FAILURE;
+			}
 
-		int init_server(char **argv)
-		{
-			this->password = argv[2];
-			this->port_number = std::atoi(argv[1]);
-			this->_socket = socket(AF_INET, SOCK_STREAM, 0);
-			if (this->_socket < 0)
-				return (this->fatal_error("socket failure"));
-			bzero((char *) &this->server_addr, sizeof(this->server_addr));
-			this->server_addr.sin_family = AF_INET;
-			this->server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-			this->server_addr.sin_port = htons(this->port_number);
-			if (bind(this->_socket, (struct sockaddr *) &this->server_addr,
-					sizeof(this->server_addr)) < 0)
-				return (this->fatal_error("bind failure"));
-			if (listen(this->_socket, 5) < 0)
-				return (this->fatal_error("listen failure"));
-			return 0;
-		}
-
-		int	client_verifying(char * cmd, Client * client)
-		{
-			int n = 0;
-			if (!cmd || *cmd == 0)
+			int init_server(char **argv)
+			{
+				this->password = argv[2];
+				this->port_number = std::atoi(argv[1]);
+				this->_socket = socket(AF_INET, SOCK_STREAM, 0);
+				if (this->_socket < 0)
+					return (this->fatal_error("socket failure"));
+				bzero((char *) &this->server_addr, sizeof(this->server_addr));
+				this->server_addr.sin_family = AF_INET;
+				this->server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+				this->server_addr.sin_port = htons(this->port_number);
+				if (bind(this->_socket, (struct sockaddr *) &this->server_addr,
+						sizeof(this->server_addr)) < 0)
+					return (this->fatal_error("bind failure"));
+				if (listen(this->_socket, 5) < 0)
+					return (this->fatal_error("listen failure"));
 				return 0;
-			if (client->verified)
-				return 1;
-			char *token;
-			string s_token;
-			token = std::strtok(cmd, " ");
-			s_token = token;
-			if (s_token == "NICK\r\n" || s_token == "NICK\n") //hundle no nickname given
-				write(client->fd_socket, ERR_NONICKNAMEGIVEN, strlen(ERR_NONICKNAMEGIVEN));
-			else if (s_token == "NICK") //set Nick_Name
-			{
-				client->nick = std::strtok(NULL, " ");
-				if (client->nick.find('\n') != string::npos)
-					client->nick = client->nick.substr(0,client->nick.size() - 1);
-				if (client->nick.find('\r') != string::npos)
-					client->nick = client->nick.substr(0,client->nick.size() - 1);
-				for (size_t i = 0; i < this->clients.size(); i++)
-				{
-					if (this->clients[i].nick == client->nick)
-						n++;
-					if (n > 1){
-						string dup = "433 * " + client->nick + ":Nickname is already in use\r\n";
-						write(client->fd_socket, dup.c_str(), strlen(dup.c_str()));
-						client->nick = "";
-						break ;
-					}
-				}
 			}
-			if (s_token == "PASS") //set Password
+
+			int	client_verifying(char * cmd, Client * client)
 			{
-				client->pass = std::strtok(NULL, " ");
-				if (client->pass.find('\n') != string::npos)
-					client->pass = client->pass.substr(0,client->pass.size() - 1);
-				if (client->pass.find('\r') != string::npos)
-					client->pass = client->pass.substr(0,client->pass.size() - 1);
+				if (!cmd || *cmd == 0)
+					return 0;
+				if (client->verified)
+					return 1;
+				char *token;
+				string s_token;
+				token = std::strtok(cmd, " ");
+				s_token = token;
+				this->_NICK(s_token, client);
+				this->_USER(s_token, client);
+				this->_PASS(s_token, client);
+				if (client->nick != "" && client->username != "" && client->pass == this->password)
+					return 1;
+				return 0;
 			}
-			if (s_token == "USER") //set User_Name
+
+			void	disconnect(size_t i, int fd)
 			{
-				client->username = std::strtok(NULL, " ");
-				if (client->username.find('\n') != string::npos)
-					client->username = client->username.substr(0,client->username.size() - 1);
-				if (client->username.find('\r') != string::npos)
-					client->username = client->username.substr(0,client->username.size() - 1);
+				cout << DISCONNECTED << this->clients[i - 4].nick << " Disconnected" << endl;
+				FD_CLR(i, &this->r_socket);
+				FD_CLR(i, &this->w_socket);
+				close(fd);
+				if (this->clients.size() != 0)
+					this->clients.erase(this->clients.begin() + i - 4);
 			}
-			if (client->nick != "" && client->username != "" && client->pass == this->password)
-				return 1;
-			return 0;
-		}
-};
+	
+			void	_NICK(string s_token, Client * client);
+			void	_USER(string s_token, Client * client);
+			void	_PASS(string s_token, Client * client);
+	};
+}
