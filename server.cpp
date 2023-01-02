5a56e4a30b0b95a02da24bc9e1b040bd8c01d76b
Taha Namir <97416765+etahae@users.noreply.github.com>
2023-01-02 13:57:06 +0100

    add : added server.cpp file for clean code

---
+++ server.cpp
@@ -0,0 +1,39 @@
+#include "server.hpp"
+
+using namespace irc;
+
+void    Server::split(char *str, string &cmd, string &res)
+{
+	char* token;
+	char* rest = NULL;
+	rest = strdup(str);
+	string arg = "";
+	unsigned long pos = 0, end_pos = 0;
+
+	token = strtok_r(rest, " ", &rest);
+	cmd = token;    //NICK || USER || PASS ...
+	arg = rest;     //the whole command after removing NICK, ...
+	if (cmd == "NICK" || cmd == "PASS" || cmd == "USER")
+	{
+		pos = arg.find_first_not_of(" : \r\n");
+		end_pos = arg.find_last_not_of(" \t\f\v\n\r");
+		if (pos != string::npos && end_pos != string::npos)
+			res = arg.substr(pos, end_pos - pos + 1);
+		else if (pos != string::npos && end_pos == string::npos)
+			res = arg.substr(pos, arg.size() - pos + 1);
+		else		//(pos == end_pos)
+			res = "";
+	}
+}
+
+size_t  Server::params_calc(string params)
+{
+    char * strToken = const_cast<char *> (params.c_str());
+    size_t count = 0;
+    strToken = strtok ( strToken, " " );
+    while ( strToken != NULL ) {
+        strToken = strtok ( NULL, " " );
+        count++;
+    }
+    return count;
+}
