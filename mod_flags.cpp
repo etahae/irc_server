5f99c34558f94d67650dd1724f954f2f7ecac38f
Ayoub Salek <asalek@e1r4p3.1337.ma>
2023-01-16 16:04:55 +0100

    update: hundle (+/-)b mode to ban and unban users from channels

--- mod_flags.cpp
+++ mod_flags.cpp
@@ -167,20 +167,26 @@ void	Server::_b(char sign, string _channel, Client * _client, string _user)
         {
             if (_user == "")
             {
-                if (sign == '+')
-                {
-                    // for (std::map<.)
-                }
+                send_msg(_client, ERR_NEEDMOREPARAMS("MODE b"));
+				return ;
             }
 			else
 			{
+				if (_user.find("!") == string::npos || _user.find("@") == string::npos)
+				{
+					send_msg(_client, ERR_NEEDMOREPARAMS("MODE b"));
+					return ;
+				}
 				int user_found = _user.find_first_of('!');
 				int ip_found = _user.find_first_of('@');
 				string user_name = _user.substr(0, user_found);
 				string ip = _user.substr(ip_found + 1, _user.size());
-				// cout << "user : " << user_name << endl;
-				// cout << "ip__ : " << ip << endl;
-				chan->second->bans.insert(std::pair<string, string> (ip, user_name));
+				if (sign == '+')
+					chan->second->bans.insert(std::pair<string, string> (ip, user_name));
+				else if (sign == '-')
+					chan->second->bans.erase(ip);
+				else
+					send_msg(_client, "501 * :Unknown MODE flag");
 			}
         }
         else
